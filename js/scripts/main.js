2e7e041e48ed08952020063598468bcb2250c793
Aqib Chattha <aqibali543@gmail.com>
2024-07-22 00:54:35 +0500

    add - html

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
         setUserName();
     } else {
         localStorage.setItem("name", myName);
-        myHeading.textContent = `Mozilla is cool, ${myName}`;
+        myHeading.textContent = `Current user is : ${myName}`;
     }
 }
 
@@ -26,9 +26,33 @@ if (!localStorage.getItem("name")) {
     setUserName();
 } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    myHeading.textContent = `Current user is : ${storedName}`;
 }
 
 myButton.onclick = () => {
     setUserName();
 };
+
+
+const list = document.createElement('ul');
+const info = document.createElement('p');
+const addItem = document.createElement('button');
+info.textContent = 'Below is a dynamic list. Click anywhere on the image to add a new list item. Click an existing list item to change its text to something else.';
+addItem.textContent = 'Add new item'
+
+document.body.appendChild(info);
+document.body.appendChild(addItem);
+document.body.appendChild(list);
+
+addItem.onclick = function() {
+  const listItem = document.createElement('li');
+  const listContent = prompt('What content do you want the list item to have?');
+  listItem.textContent = listContent;
+  list.appendChild(listItem);
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    const listContent = prompt('Enter new content for your list item');
+    this.textContent = listContent;
+  }
+}
