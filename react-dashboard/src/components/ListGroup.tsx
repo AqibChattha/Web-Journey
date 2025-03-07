e9aa6adbef768f4630bae1d452fbf8c728d494c4
AqibChattha <aqibali543@gmail.com>
2025-03-07 15:37:07 +0500

    select item using useState hook

--- react-dashboard/src/components/ListGroup.tsx
+++ react-dashboard/src/components/ListGroup.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 function ListGroup() {
-  const items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  let items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
