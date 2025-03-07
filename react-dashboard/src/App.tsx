e8446a78e8b5b28104468c8035b1d4b286cf4aeb
AqibChattha <aqibali543@gmail.com>
2025-03-07 15:50:16 +0500

    passing functions via props

--- react-dashboard/src/App.tsx
+++ react-dashboard/src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["Item 1", "Item 2", "Item 3", "Item 4"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Items" />
+      <ListGroup
+        items={items}
+        heading="Items"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
