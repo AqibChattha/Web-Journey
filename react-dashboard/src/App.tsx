cbcd38a06177faf3ac129cbc2ba5c5c3947ecc99
AqibChattha <aqibali543@gmail.com>
2025-03-08 23:20:02 +0500

    Bootstrap Navbar

--- react-dashboard/src/App.tsx
+++ react-dashboard/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [alertVisible, setAlertVisible] = useState(false);
 
   return (
     <div>
+      <NavBar />
       {alertVisible && (
         <Alert onClose={() => setAlertVisible(false)}>Hello World</Alert>
       )}
