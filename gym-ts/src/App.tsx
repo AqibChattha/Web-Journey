a0687ce5995d3d4ba8d747ce82fa14918ea9c852
Aqib Chattha <aqibali543@gmail.com>
2025-03-11 03:33:49 +0500

    gym app in ts

---
+++ gym-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Navbar from "./scenes/navbar";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+
+  return (
+    <div className="App bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
+}
+
+export default App;
